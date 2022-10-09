504f2262ed6d4f222b20e7c44d4e33a6f1a91a72
--replace-all <al.gadzhalov@gmail.com>
2022-10-09 12:40:29 +0300

    Added: borrow and return a book functionality

---
+++ BookBorrow.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import "./BookLibrary.sol";
+
+contract BookBorrow is BookLibrary {
+
+    struct HistoryAddressSet {
+        address[] addresses;
+        mapping (address => bool) isIn;
+    }
+
+    mapping(uint32 => HistoryAddressSet) historyAddresses;
+
+    event BookBorrowedEvent(string name, string author);
+    event BookReturnEvent(string name, string author);
+    
+    function borrowABook(uint32 _bookId) external onlyExistingBookIds(_bookId) {
+        require (availableBooks[_bookId] > 0, "Book copy is not available.");
+        require (borrowedBooks[msg.sender][_bookId] == false, "You can borrow only one copy of this book");
+        borrowedBooks[msg.sender][_bookId] = true;
+        availableBooks[_bookId] = availableBooks[_bookId] - 1;
+        addAddressBorrowedABook(_bookId, msg.sender);
+        emit BookBorrowedEvent(books[_bookId].name, books[_bookId].author);
+    }
+
+    function returnBook(uint32 _bookId) external onlyExistingBookIds(_bookId) {
+        require (borrowedBooks[msg.sender][_bookId] == true, "You didn't borrow this book");
+        borrowedBooks[msg.sender][_bookId] = false;
+        availableBooks[_bookId] = availableBooks[_bookId] + 1;
+        emit BookReturnEvent(books[_bookId].name, books[_bookId].author);
+    }
+
+    /**
+     * @notice restricts address repetitiveness for borrowed book.
+     * In case an address borrowed the same book multiple times.
+     */
+    function addAddressBorrowedABook(uint32 _bookId, address _address) private {
+        if (!historyAddresses[_bookId].isIn[_address]) {
+            historyAddresses[_bookId].addresses.push(_address);
+            historyAddresses[_bookId].isIn[_address] = true;
+        }
+    }
+
+}
