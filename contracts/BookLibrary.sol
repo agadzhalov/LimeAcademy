1a4e18c3209fb7dc22fb86fb9cb660e251bd688d
--replace-all <al.gadzhalov@gmail.com>
2022-10-17 20:04:13 +0300

    Removed: for loop for optimization

--- contracts/BookLibrary.sol
+++ contracts/BookLibrary.sol
@@ -17,24 +17,13 @@ contract BookLibrary is Ownable {
     Book[] public books;
 
     mapping(uint32 => uint32) public availableBooks;
+    mapping(string => bool) public isBookAdded;
     mapping(address => mapping(uint32 => bool)) public borrowedBooks; 
 
     event BookAddedEvent(string name, string author, uint32 copies);
 
-    /**
-    *   @notice restricts adding the same book multiple times
-    *
-    *   TO IMPROVE: Highly ineffective to iterate every time through all the books
-    */
-    modifier onlyUniqueBooks(string memory _name, string memory _author) {
-        bool isAdded = false;
-        for (uint32 i = 0; i < books.length; i++) {
-            // books can have the same name but differrent authors
-            if (keccak256(abi.encodePacked(books[i].name)) == keccak256(abi.encodePacked(_name)) &&
-                keccak256(abi.encodePacked(books[i].author)) == keccak256(abi.encodePacked(_author)))
-                isAdded = true;
-        }
-        require (!isAdded, "Book already added");
+    modifier onlyUniqueBooks(string memory _name) {
+        require (!isBookAdded[_name], "Book already added");
         _;
     }
 
@@ -43,11 +32,12 @@ contract BookLibrary is Ownable {
         _;
     }
 
-    function addNewBook(string memory _name, string memory _author, uint32 _copies) public onlyOwner onlyUniqueBooks(_name, _author) {
+    function addNewBook(string memory _name, string memory _author, uint32 _copies) public onlyOwner onlyUniqueBooks(_name) {
         require (_copies > 0, "New books' copies must be more than zero");
         uint32 _bookId = uint32(books.length);
         books.push(Book(_name, _author, _bookId, _copies));
         availableBooks[_bookId] = _copies;
+        isBookAdded[_name] = true;
         emit BookAddedEvent(_name, _author, _copies);
     }
 
