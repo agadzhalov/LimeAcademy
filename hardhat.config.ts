54a710a09d3aaa0acd38cb6a53d62e1b22935f11
--replace-all <al.gadzhalov@gmail.com>
2022-10-18 22:50:39 +0300

    Renamed deploy scritps

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig, subtask, task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import  deployBookLibraryContract  from './scripts/deploy-goerli';
+import  deployLocally  from './scripts/deploy-localhost';
 
 import * as dotenv from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
@@ -32,6 +33,11 @@ const config: HardhatUserConfig = {
 };
 
 export default config;
+
+task("deploy-localhost", "Deploys contract on Goerli network")
+  .setAction(async(args: any, hre: HardhatRuntimeEnvironment) => {
+    await deployLocally();
+  });
 
 task("deploy-goerli", "Deploys contract on Goerli network")
   .setAction(async(args: any, hre: HardhatRuntimeEnvironment) => {
--- scripts/deploy-localhost.ts
+++ scripts/deploy-localhost.ts
@@ -1,13 +1,8 @@
-import { ethers } from "hardhat";
-
-async function main() {
+const deployLocally = async() => {
   const BookUtils = await ethers.getContractFactory("BookUtils");
   const bookUtils = await BookUtils.deploy();
   await bookUtils.deployed();
   console.log("BookUtils deployed to:", bookUtils.address);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+export default deployLocally;
