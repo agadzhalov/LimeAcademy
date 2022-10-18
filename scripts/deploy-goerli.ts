d779472b54395c64d780bf05fc600e2c54561079
--replace-all <al.gadzhalov@gmail.com>
2022-10-18 13:43:42 +0300

    Changed: spaces

--- scripts/deploy-goerli.ts
+++ scripts/deploy-goerli.ts
@@ -4,9 +4,11 @@ const deployBookLibraryContract = async(args: any, hre: HardhatRuntimeEnvironmen
   await hre.run("compile");
   const [deployer] = await ethers.getSigners();
   await hre.run("print", { message: "Depoying with address: " + deployer.address + "\n" + "Balance: " + (await deployer.getBalance()).toString()});
+
   const BookUtils = await ethers.getContractFactory("BookUtils");
   const bookUtils = await BookUtils.deploy();
   await bookUtils.deployed();
+  
   await hre.run("print", { message: "BookUtils deployed to: " + bookUtils.address});
 }
 
