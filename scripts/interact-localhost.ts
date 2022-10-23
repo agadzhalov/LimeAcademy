c8a73242dd9f8903cc201b0648ae50893049901a
--replace-all <al.gadzhalov@gmail.com>
2022-10-23 23:15:32 +0300

    Added: interact-localhost all requirements met

--- scripts/interact-localhost.ts
+++ scripts/interact-localhost.ts
@@ -11,6 +11,7 @@ const interactLocally = (async() => {
     /**
      * WALLET
      */
+    // @TODO import from env
     const wallet = new ethers.Wallet("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80", provider);
     const balance = await wallet.getBalance();
     console.log(balance.toString())
@@ -19,20 +20,85 @@ const interactLocally = (async() => {
     /**
      * CONTRACT
      */
+    // @TODO import from env
     const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
     const bookUtilsContract = new ethers.Contract(contractAddress, BookUtils.abi, wallet);
 
     // 1. Creates book
-    const addNewBook = await bookUtilsContract.addNewBook("The Godfather", "Mario Puzo", 5);
+    await addNewBook(bookUtilsContract, "The Godfather", "Mario Puzo", 5).catch((error) => {});
+    await addNewBook(bookUtilsContract, "Hooked", "Nir Eyal", 1).catch((error) => {});
+
+    // 2. Checks all available books
+    await checkAvailableBooks(bookUtilsContract);
+
+    // 3. Rents a book
+    await borrowABook(bookUtilsContract, getBookHashedId("Hooked", "Nir Eyal")).catch((error) => {});
+
+    // 4. Checks if a book is borrowed by ID
+    const address = (await wallet.getAddress()).toString();
+    await checkIfBorrowedByBookId(bookUtilsContract, address, getBookHashedId("Hooked", "Nir Eyal")).catch((error) => {});
+
+    // 5. Returns a book
+    await returnABook(bookUtilsContract, getBookHashedId("Hooked", "Nir Eyal")).catch((error) => {});
+
+    // 6. Checks availability of a book
+    await checkAvailabilityOfBookById(bookUtilsContract, getBookHashedId("Hooked", "Nir Eyal")).catch((error) => {});
+});
+
+interactLocally();
+
+const addNewBook = (async(contract: any, name: string, author: string, copies: number) => {
+    const addNewBook = await contract.addNewBook(name, author, copies);
     const addNewBookTx = await addNewBook.wait();
     if (addNewBookTx.status != 1) {
         console.log("Transaction failed");
-        return;
     }
+    console.log("Book Added: " + name + ", " + author);
+});
 
-    // 2. Checks all available books
-    const checkAvailableBooks = await bookUtilsContract.showAvailableBooks();
-    console.log(await checkAvailableBooks);
-})
+const checkAvailableBooks = (async(contract: any) => {
+    const checkAvailableBooks = await contract.showAvailableBooks();
+    console.log("Books availability: ", await checkAvailableBooks);
+});
 
-interactLocally();
+const borrowABook = (async(contract: any, bookId: string) => {
+    const borrowBook = await contract.borrowABook(bookId);
+    const borrowBookTx = await borrowBook.wait();
+    if (borrowBookTx.status != 1) {
+        console.log("Transaction failed");
+    }
+    console.log("Book rented: " + bookId);
+});
+
+const checkIfBorrowedByBookId = (async(contract: any, address: string, bookId: string) => {
+    const isBookBorrowedBy = await contract.borrowedBooks(address, bookId);
+    console.log(isBookBorrowedBy  ? "YES, book is borrowed by " : "NO, book is NOT borrowed by", address);
+});
+
+const returnABook = (async(contract: any, bookId: string) => {
+    const returnABook = await contract.returnBook(bookId);
+    const returnABookTx = await returnABook.wait();
+    if (returnABookTx.status != 1) {
+        console.log("Transaction failed");
+    }
+    console.log("Book returned: " + bookId);
+});
+
+
+/**
+ * @TODO think more how to handle when book is not in the list
+ * p.s. if not exisiting always returns 0 index
+ */
+ const checkAvailabilityOfBookById = (async(contract: any, bookId: string) => {
+    const bookIndex = await contract.availableIdToIndex(bookId);
+    const book = await contract.allBooks(bookIndex);
+    console.log(book.name, book.author, "| Available copies: " + book.copies, );
+});
+
+const getBookHashedId =  (name: string, author: string) : string => {
+    const bookId = ethers.utils.keccak256(ethers.utils.concat([
+        ethers.utils.toUtf8Bytes(name), 
+        ethers.utils.toUtf8Bytes(author)
+    ]));
+    return bookId;
+}
