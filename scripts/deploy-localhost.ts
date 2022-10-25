ebf6946dcf8dee29163cb4eed36dd3e9f0362ff5
--replace-all <al.gadzhalov@gmail.com>
2022-10-25 18:04:21 +0300

    Added: LimeAcademy WebApp boilerplate

--- scripts/deploy-localhost.ts
+++
@@ -1,8 +0,0 @@
-const deployLocally = async() => {
-  const BookUtils = await ethers.getContractFactory("BookUtils");
-  const bookUtils = await BookUtils.deploy();
-  await bookUtils.deployed();
-  console.log("BookUtils deployed to:", bookUtils.address);
-}
-
-export default deployLocally;
